9af5678f5eb718f1576b3a5571a45f213f7a4bc5
liliia_vintiuk <rudapanda2016@gmail.com>
2024-02-26 22:36:24 +0200

    ne vyksta

--- js/create.js
+++ js/create.js
@@ -1,26 +1,20 @@
-
 const kaitlentestructuraDOM = document.getElementById('kaitlentestructura');
 const mygtukastructuraDOM = document.getElementById('mygtukastructura');
 const btnDOM = document.getElementById('btn');
 const vklDOM = document.getElementById('vkl');
-const circlesDOM = document.querySelectorAll('.circle');
 
-let HTML = ' ';
+let countcircle = 1;
+let HTML = '<div class="circle">1</div>';
+
 function addCircle() {
-    
-    HTML += `<div id="circle" class="circle"></div>`;
+    HTML += `<div class="circle">${++countcircle}</div>`;
     kaitlentestructuraDOM.innerHTML = HTML;
 }
-let HTML2 = ' ';
+let countmygtuka = 1;
+let HTML2 = '<button id="vkl" class="vkl">1</button>';
 function addMygtuka() {
-    
-    HTML2 += `<button id="vkl" class="vkl">*</button>`;
+    HTML2 += `<button id="vkl" class="vkl">${++countmygtuka}</button>`;
     mygtukastructuraDOM.innerHTML = HTML2;
-}
-function changeColorCircles() {
-   circlesDOM.forEach(circle => {
-        circle.style.backgroundColor = "red";
-    });
 }
 
 btnDOM.addEventListener('click', function () {
@@ -28,4 +22,12 @@ btnDOM.addEventListener('click', function () {
     addCircle();
 });
 
+
+
+function changeColorCircles() {
+    const circleDOM = document.querySelector('.circle');
+    circleDOM.style.backgroundColor = "red";
+    circleDOM.style.transition = "background-color 2s ease"; 
+    
+}
 vklDOM.addEventListener('click', changeColorCircles);
